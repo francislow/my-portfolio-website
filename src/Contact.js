ee5a7dbe7f3a268d153c0e611131693202d87105
Francis <francislow_6@hotmail.com>
2019-12-25 02:25:29 +0800

    Implemented workable contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,26 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import insta_icon from "./images/insta-icon.png";
 import github_icon from "./images/github-icon.png";
 import linkedin_icon from "./images/linkedin-icon.png";
-import php_form from "./contactform.php"
 
 function Contact() {
+  const [senderName, setSenderName] = useState("");
+  const [senderMsg, setSenderMsg] = useState("");
+  const [senderEmail, setSenderEmail] = useState("");
+  const [senderSubject, setSenderSubject] = useState("");
+
+  function handleSubmit() {
+    const templateId = "template_KeL9SE3G";
+
+    sendFeedback(templateId, {
+      message_html: senderMsg,
+      subject_html: senderSubject,
+      from_name: senderName,
+      reply_to: senderEmail
+    });
+  }
+
+  function sendFeedback(templateId, variables) {
+    window.emailjs
+      .send("gmail", templateId, variables)
+      .then(res => {
+        console.log("Email successfully sent!");
+      })
+      .catch(err =>
+        console.error(
+          "Oh well, you failed. Here some thoughts on the error that occured:",
+          err
+        )
+      );
+  }
+
   return (
     <div className="wrapper-page-content contact-page">
       <p className="page-title m-black-font">CONTACT</p>
       <div className="wrapper-form">
-        <form className="mail-form" action={php_form} method="post">
-          <input type="text" name="name" placeholder="Full Name" />
-          <input type="text" name="email" placeholder="Email" />
-          <input type="text" name="subject" placeholder="Subject" />
-          <textarea name="message" placeholder="Message" />
-          <button type="submit" name="submit">Send email!</button>
+        <form className="mail-form">
+          <input
+            type="text"
+            placeholder="Full Name"
+            onChange={e => setSenderName(e.target.value)}
+            value={senderName}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={e => setSenderEmail(e.target.value)}
+            value={senderEmail}
+          />
+          <input
+            type="text"
+            placeholder="Subject"
+            onChange={e => setSenderSubject(e.target.value)}
+            value={senderSubject}
+          />
+          <textarea
+            placeholder="Message"
+            onChange={e => setSenderMsg(e.target.value)}
+            value={senderMsg}
+          />
+          <button type="submit" onClick={() => handleSubmit()}>
+            Send email!
+          </button>
         </form>
       </div>
       <div className="wrapper-icons">
-        <a target="_blank" href="https://www.instagram.com/friendcislol/"><img className="contact-profile-icon" src={insta_icon} alt="Insta Icon"/></a>
-        <a target="_blank" href="https://github.com/francislow"><img className="contact-profile-icon" src={github_icon} alt="Github Icon"/></a>
-        <a target="_blank" href="https://www.linkedin.com/in/francis-low-bb1b4079/"><img className="contact-profile-icon" src={linkedin_icon} alt="Linkedin Icon"/></a>
+        <a target="_blank" href="https://www.instagram.com/friendcislol/">
+          <img
+            className="contact-profile-icon"
+            src={insta_icon}
+            alt="Insta Icon"
+          />
+        </a>
+        <a target="_blank" href="https://github.com/francislow">
+          <img
+            className="contact-profile-icon"
+            src={github_icon}
+            alt="Github Icon"
+          />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/francis-low-bb1b4079/"
+        >
+          <img
+            className="contact-profile-icon"
+            src={linkedin_icon}
+            alt="Linkedin Icon"
+          />
+        </a>
       </div>
     </div>
   );
--- src/contactform.php
+++ src/contactform.php
@@ -11,5 +11,5 @@ if (isset($_POST['submit'])) {
 
 
     mail($my_email, $subject, $text, "test");
-    header("Location: index.php?mailsend");
+    header("Location: index.html?mailsend");
 }
