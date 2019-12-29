70124781910c4126ac1137e02bb22aa62bec795e
Francis <francislow_6@hotmail.com>
2019-12-29 17:57:13 +0800

    Implement email sent result notification

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function ContactForm() {
   const [senderName, setSenderName] = useState("");
   const [senderMsg, setSenderMsg] = useState("");
   const [senderEmail, setSenderEmail] = useState("");
   const [senderSubject, setSenderSubject] = useState("");
+  const [modelBoxText, setModelBoxText] = useState("");
 
+  const modelRef = useRef(null);
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     const templateId = "template_KeL9SE3G";
     sendFeedBack(templateId, {
@@ -16,67 +18,93 @@ function ContactForm() {
       from_name: senderName,
       reply_to: senderEmail
     });
+
+    refreshPage();
   }
 
   function sendFeedBack(templateId, variables) {
     window.emailjs
       .send("gmail", templateId, variables)
       .then(res => {
-        console.log("Email successfully sent!");
-        refreshPage();
+        activateResultNotification("Email successfully sent!");
       })
-      .catch(err =>
+      .catch(err => {
         console.error(
           "Oh well, you failed. Here some thoughts on the error that occured:",
           err
-        )
-      );
+        );
+        activateResultNotification("Error, email was not sent successfully");
+      });
+  }
+
+  function activateResultNotification(displayText) {
+    setModelBoxText(displayText);
+    setModelVisibility(true);
+  }
+
+  function setModelVisibility(isVisible) {
+    isVisible ? 
+    modelRef.current.className = modelRef.current.className.replace(
+      "invisible",
+      "visible"
+    ) : 
+    modelRef.current.className = modelRef.current.className.replace(
+      "visible",
+      "invisible"
+    )
+
   }
 
   function refreshPage() {
-    window.location.href = "http://francislow.netlify.com/";
+    window.location.href = "https://francislow.netlify.com/#contact"
   }
 
   return (
-    <div className="wrapper-form">
-      <form>
-        <input
-          className="form-detail"
-          type="text"
-          placeholder="Full Name"
-          onChange={e => setSenderName(e.target.value)}
-          value={senderName}
-        />
-        <input
-          className="form-detail"
-          type="text"
-          placeholder="Email"
-          onChange={e => setSenderEmail(e.target.value)}
-          value={senderEmail}
-        />
-        <input
-          className="form-detail"
-          type="text"
-          placeholder="Subject"
-          onChange={e => setSenderSubject(e.target.value)}
-          value={senderSubject}
-        />
-        <textarea
-          className="form-detail form-message"
-          type="text"
-          placeholder="Message"
-          onChange={e => setSenderMsg(e.target.value)}
-          value={senderMsg}
-        />
-        <button
-          className="send-email-btn"
-          type="submit"
-          onClick={(e) => handleSubmit(e)}
-        >
-          Send email
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="wrapper-form">
+        <form>
+          <input
+            className="form-detail"
+            type="text"
+            placeholder="Full Name"
+            onChange={e => setSenderName(e.target.value)}
+            value={senderName}
+          />
+          <input
+            className="form-detail"
+            type="text"
+            placeholder="Email"
+            onChange={e => setSenderEmail(e.target.value)}
+            value={senderEmail}
+          />
+          <input
+            className="form-detail"
+            type="text"
+            placeholder="Subject"
+            onChange={e => setSenderSubject(e.target.value)}
+            value={senderSubject}
+          />
+          <textarea
+            className="form-detail form-message"
+            type="text"
+            placeholder="Message"
+            onChange={e => setSenderMsg(e.target.value)}
+            value={senderMsg}
+          />
+          <button
+            className="send-email-btn"
+            type="submit"
+            onClick={e => handleSubmit(e)}
+          >
+            Send email
+          </button>
+        </form>
+      </div>
+      <div ref={modelRef} className="email-model-box invisible">
+        <span onClick={() => setModelVisibility(false)}>&times;</span>
+        {modelBoxText}
+      </div>
+    </>
   );
 }
 
