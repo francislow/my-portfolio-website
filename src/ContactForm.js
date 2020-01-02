0e1cdb4ea3a3e98548f0760e7f6494f29f948ea1
Francis <francislow_6@hotmail.com>
2020-01-02 14:48:06 +0800

    Add email requirements

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,26 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function ContactForm() {
   const [senderName, setSenderName] = useState("");
-  const [senderMsg, setSenderMsg] = useState("");
-  const [senderEmail, setSenderEmail] = useState("");
-  const [senderSubject, setSenderSubject] = useState("");
-  const [modelBoxText, setModelBoxText] = useState("");
+  const [namePlaceholder, setNamePlaceholder] = useState("Name");
+  const [nameColour, setNameColour] = useState("");
 
+  const [senderMsg, setSenderMsg] = useState("");
+  const [msgPlaceholder, setMsgPlaceholder] = useState("Message");
+  const [msgColour, setMsgColour] = useState("");
+
+  const [senderEmail, setSenderEmail] = useState("");
+  const [emailPlaceholder, setEmailPlaceholder] = useState("Email");
+  const [emailColour, setEmailColour] = useState("");
+
+  const [senderSubject, setSenderSubject] = useState("");
+  const [subjectPlaceholder, setSubjectPlaceholder] = useState("Subject");
+  const [subjectColour, setSubjectColour] = useState("");
+
+  const [modelBoxText, setModelBoxText] = useState("");
   const [modelVisibility, setModelVisiblility] = useState("invisible");
+
+  useEffect(() => {
+    if (senderName !== "") {
+      setNamePlaceholder("Name");
+      setNameColour("");
+    }
+    if (senderEmail !== "") {
+      setEmailPlaceholder("Email");
+      setEmailColour("");
+    }
+    if (senderMsg !== "") {
+      setMsgPlaceholder("Message");
+      setMsgColour("");
+    }
+    if (senderSubject !== "") {
+      setSubjectPlaceholder("Subject");
+      setSubjectColour("");
+    }
+  }, [senderName, senderSubject, senderMsg, senderEmail]);
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    const templateId = "template_KeL9SE3G";
-    sendFeedBack(templateId, {
-      message_html: senderMsg,
-      subject_html: senderSubject,
-      from_name: senderName,
-      reply_to: senderEmail
-    });
+    let isFormCompleted = true;
+    if (senderName === "") {
+      setNamePlaceholder("Please include your name");
+      setNameColour("red");
+      isFormCompleted = false;
+    }
+    if (senderEmail === "") {
+      setEmailPlaceholder("Please include your email");
+      setEmailColour("red");
+      isFormCompleted = false;
+    }
+    if (senderMsg === "") {
+      setMsgPlaceholder("Please include a Message");
+      setMsgColour("red");
+      isFormCompleted = false;
+    }
+    if (senderSubject === "") {
+      setSubjectPlaceholder("Please include a subject");
+      setSubjectColour("red");
+      isFormCompleted = false;
+    }
 
-    refreshPage();
+    if (isFormCompleted === true) {
+      const templateId = "template_KeL9SE3G";
+      sendFeedBack(templateId, {
+        message_html: senderMsg,
+        subject_html: senderSubject,
+        from_name: senderName,
+        reply_to: senderEmail
+      });
+      refreshPage();
+    }
   }
 
   function sendFeedBack(templateId, variables) {
@@ -52,30 +105,30 @@ function ContactForm() {
       <div className="wrapper-form">
         <form>
           <input
-            className="form-detail"
+            className={`form-detail ${nameColour}`}
             type="text"
-            placeholder="Name"
+            placeholder={namePlaceholder}
             onChange={e => setSenderName(e.target.value)}
             value={senderName}
           />
           <input
-            className="form-detail"
+            className={`form-detail ${emailColour}`}
             type="text"
-            placeholder="Email"
+            placeholder={emailPlaceholder}
             onChange={e => setSenderEmail(e.target.value)}
             value={senderEmail}
           />
           <input
-            className="form-detail"
+            className={`form-detail ${subjectColour}`}
             type="text"
-            placeholder="Subject"
+            placeholder={subjectPlaceholder}
             onChange={e => setSenderSubject(e.target.value)}
             value={senderSubject}
           />
           <textarea
-            className="form-detail form-message"
+            className={`form-detail form-message ${msgColour}`}
             type="text"
-            placeholder="Message"
+            placeholder={msgPlaceholder}
             onChange={e => setSenderMsg(e.target.value)}
             value={senderMsg}
           />
